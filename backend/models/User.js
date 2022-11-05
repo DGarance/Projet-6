f7581732e4b75baca2a77008f77ec9b2cd33e88f
Garance Defrel <garance972@hotmail.com>
2022-11-05 23:36:06 +0100

    modif

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,7 +7,7 @@ const uniqueValidator = require("mongoose-unique-validator");
 //Création du schéma User
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  passwword: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 //On utilise le plugin unique-validator pour éviter les doublons d'utilisateurs
